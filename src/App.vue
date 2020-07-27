b7d79cf6cc861a350d20f894daa2430e9a4bfc02
matuken_3 <k.matutaka.biz@gmail.com>
2020-07-27 20:46:41 +0900

    create disp todolist

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div>
+    <h3>My Todo</h3>
+    <input type="text" v-model="newTodo" placeholder="Input here…" />
+    <button @click="addTodo()">ADD</button>
+    <h5>Todo List</h5>
+    <ul>
+      <li v-for="(todo, i) in todos" v-bind:key="i">
+        {{ todo }}
+        <button @click="deleteTodo(i)">DEL</button>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo === "") {
+        return;
+      }
+      this.todos.push(this.newTodo);
+      this.newTodo = "";
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
